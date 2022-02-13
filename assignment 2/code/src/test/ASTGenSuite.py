4f3bc1158999fd7a9730b4d88e16dfd3ee2def72
CSEK19 <70205271+CSEK19@users.noreply.github.com>
2022-02-13 23:43:39 +0700

    Update at 23:43 13/02

--- assignment 2/code/src/test/ASTGenSuite.py
+++ assignment 2/code/src/test/ASTGenSuite.py
@@ -4,7 +4,7 @@ from AST import *
 
 class ASTGenSuite(unittest.TestCase):
    def test_300(self):
-       input = '''Class _j{Constructor (_,_4m_q_R6r:_){}Var $o:String ;Val $3O:H=_::$_;}'''
+       input = '''Class A{foo(){Self::$a = 1;}}'''
        expect = '''Program([ClassDecl(Id(_j),[MethodDecl(Id(Constructor),Instance,[param(Id(_),ClassType(Id(_))),param(Id(_4m_q_R6r),ClassType(Id(_)))],Block([],[])),AttributeDecl(Static,VarDecl(Id($o),StringType)),AttributeDecl(Static,ConstDecl(Id($3O),ClassType(Id(H)),FieldAccess(Id(_),Id($_))))])])'''
        self.assertTrue(TestAST.test(input,expect,300))
 
